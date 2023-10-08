99595dd8696885a9c3c1658d872917bcfbec6d33
Vladimir Lazić <contact@vlazic.com>
2023-10-08 21:45:10 +0200

    feat: enhance help command with sample usage

--- src/cli.js
+++ src/cli.js
@@ -10,6 +10,20 @@ program
     "Sort results in ascending or descending order (asc/desc). Default is asc.",
   )
   .option("--debug", "Display debug logs")
+  .usage("[options] <command> [...]")
+  .on("--help", () => {
+    console.log("");
+    console.log("Examples:");
+    console.log(
+      "  $ git-task-analyzer --user john.doe --repo ./my-project --sort desc",
+    );
+    console.log(
+      "  $ git-task-analyzer -u john.doe -r ./my-project -o tasks.json",
+    );
+    console.log(
+      "  $ git-task-analyzer -r ./my-project --exclude yarn.lock,package-lock.json",
+    );
+  })
   .parse(process.argv);
 
 const CLIOptions = program.opts();
